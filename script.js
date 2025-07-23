a38656e521e1525334ce7bd37e4b88d94ffa298f
uqerik <erichils10@gmail.com>
2025-07-23 15:05:08 +0100

    Task 2

---
+++ script.js
@@ -0,0 +1,13 @@
+let upskillProfile = {
+      name: "Chukwuma Ugwu (WD)",
+      age: 25,
+      myAbout: function introduceYourself(){
+        console.log ("Hi, my name is " + this.name);
+      },
+      hobbies: [" Football", "Reading", " Research"],
+     };
+     
+     upskillProfile.myAbout()
+     console.log (upskillProfile.hobbies[1]);
+
+     console.log("Callback function has been really diffcult for me to understand")
