e7df80ac439e2cbc52b97f140f9aacd4aca3e132
uqerik <erichils10@gmail.com>
2025-07-28 21:03:40 +0100

    Task 7

---
+++ food-menu.js
@@ -0,0 +1,20 @@
+ //Responsive Nav bar
+
+ function showSidebar(){
+        const sidebar = document.querySelector(".sidebar");
+        sidebar.style.display = "flex";
+      }
+
+      function hideSidebar(){
+        const hide = document.querySelector(".sidebar");
+        hide.style.display = "none";
+      }
+   
+  
+
+
+
+
+
+
+
